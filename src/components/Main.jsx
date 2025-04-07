1a2841a9c6e34c1a8726028b4a69ca7af12a62f7
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-04-07 19:49:53 +0100

    fix the code: - change the form to a dive to fix the sumbition because we dont need it - add some padding to the footer from the left and the right in the mobile version

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,30 +2,68 @@ import { useState, useEffect } from 'react';
 
 export default function Main() {
   const [meme, setMeme] = useState({
-    topText: 'top text',
-    buttonText: 'buttom text',
+    topText: 'One does not simply',
+    bottomText: 'Walk into Mordor',
     imageUrl: 'http://i.imgflip.com/1bij.jpg',
   });
+
+  const [allMemeImage, setAllMemeImage] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((res) => res.json())
+      .then((data) => setAllMemeImage(data.data.memes));
+  }, []);
+
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function getRandomImage() {
+    const randomImage = Math.floor(Math.random() * allMemeImage.length);
+    setMeme((prev) => ({
+      ...prev,
+      imageUrl: allMemeImage[randomImage].url,
+    }));
+  }
+
   return (
     <main>
       <h3>Welcome to the Meme Crafter Website</h3>
-      <form>
+      <div className="form">
         <div className="input-row">
           <label>
             Top Text
-            <input></input>
+            <input
+              type="text"
+              name="topText"
+              placeholder="One does not simply"
+              onChange={handleChange}
+              value={meme.topText}
+            ></input>
           </label>
+
           <label>
             Bottom Text
-            <input></input>
+            <input
+              type="text"
+              name="bottomText"
+              placeholder="Walk into Mordor"
+              onChange={handleChange}
+              value={meme.bottomText}
+            ></input>
           </label>
         </div>
-        <button>Get a new meme image</button>
-      </form>
+        <button onClick={getRandomImage}>Get a new meme image</button>
+      </div>
       <div className="meme">
         <img src={meme.imageUrl}></img>
         <span className="top-text">{meme.topText}</span>
-        <span className="buttom-text">{meme.buttonText}</span>
+        <span className="buttom-text">{meme.bottomText}</span>
       </div>
     </main>
   );
