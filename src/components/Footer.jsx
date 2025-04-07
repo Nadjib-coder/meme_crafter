47f93b8d224c93821082417f3d56b16f7aea494f
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-04-07 15:22:46 +0100

    complet all the structure and styling : - complet the responsive version for mobile and tablet - complet the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        © 2025 <a href="https://nadjibmouhoun.me/">Nadjib Mouhoun</a>{' '}
+        development. All rights reserved.
+      </p>
+    </footer>
+  );
+}
